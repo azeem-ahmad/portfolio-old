1c1f26326ee22c0f08fcb084f399c1c0137bea12
Azeem Ahmad <41817419+azeem-ahmad@users.noreply.github.com>
2021-01-04 15:19:32 +0530

    Add files via upload

---
+++ precache-manifest.89d56ff487cea4af5d70f947d0b1ccbb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd1a175d2d2ff8a819ad8ed22e900fd",
+    "url": "/portfolio-old/index.html"
+  },
+  {
+    "revision": "d5c1e373973d93d573bc",
+    "url": "/portfolio-old/static/css/main.6426c58f.chunk.css"
+  },
+  {
+    "revision": "d2f39ad22712fc25fa92",
+    "url": "/portfolio-old/static/js/2.244f3606.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-old/static/js/2.244f3606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c1e373973d93d573bc",
+    "url": "/portfolio-old/static/js/main.5c125228.chunk.js"
+  },
+  {
+    "revision": "e7721c5413f5b744bc87",
+    "url": "/portfolio-old/static/js/runtime-main.eaf292d1.js"
+  },
+  {
+    "revision": "c1676b76a0101336db7c6f58b3d296ff",
+    "url": "/portfolio-old/static/media/azeem.c1676b76.jpg"
+  },
+  {
+    "revision": "4b443abd5b96d87395de308b05365cc7",
+    "url": "/portfolio-old/static/media/bg_elements.4b443abd.png"
+  },
+  {
+    "revision": "ad272e2a5b0f146e31be256c17785a03",
+    "url": "/portfolio-old/static/media/contact-1.ad272e2a.png"
+  },
+  {
+    "revision": "55354fb874be2757f85adc25bc0369cd",
+    "url": "/portfolio-old/static/media/hero-bg.55354fb8.png"
+  }
+]);
